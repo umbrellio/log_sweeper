2eb8d2d4c5b0eada5fb28f8bf23c227adf99c159
Yuri Smirnov <tycooon@yandex.ru>
2019-02-03 12:25:17 +0300

    add specs

--- lib/log_sweeper.rb
+++ lib/log_sweeper.rb
@@ -2,34 +2,43 @@
 
 require "log_sweeper/version"
 
-# require "shellwords"
-# require "pathname"
+require "shellwords"
+require "pathname"
+require "logger"
 
 module LogSweeper
-  def self.run(path, logs_lifetime_days_count: 10)
+  extend self
+
+  def run(path, logs_lifetime_days_count: 10, logger: Logger.new(STDOUT))
     min_mtime = Time.now - (logs_lifetime_days_count.to_f * 24 * 3600)
 
     Pathname.new(path).each_child do |entry|
-      filename = entry.basename.to_s
-      next unless entry.file? && filename.match?(/\.log\b/)
+      next unless entry.file?
 
-      if entry.mtime < min_mtime
-        puts "deleting #{entry}"
+      filename = entry.basename.to_s
+
+      if filename.match?(/\.log\b/) && entry.mtime < min_mtime
+        logger.info "deleting #{entry}"
         entry.delete
       elsif filename.match?(/\.log\.\d+$/)
-        puts "gzipping #{entry}"
-        system "gzip #{Shellwords.escape(entry)}"
+        logger.info "gzipping #{entry}"
+        compress_file(entry)
+        entry.delete
+      else
+        logger.info "skipping #{entry}"
       end
     end
 
     true
   end
 
-  # def compress_file(file_name)
-  #   zipped = "#{file_name}.gz"
+  private
 
-  #   Zlib::GzipWriter.open(zipped) do |gz|
-  #     gz.write IO.binread(file_name)
-  #   end
-  # end
+  def compress_file(file_name)
+    zipped = "#{file_name}.gz"
+
+    Zlib::GzipWriter.open(zipped) do |gz|
+      gz.write IO.binread(file_name)
+    end
+  end
 end
--- spec/log_sweeper_spec.rb
+++ spec/log_sweeper_spec.rb
@@ -4,4 +4,48 @@ RSpec.describe LogSweeper do
   it "has a version number" do
     expect(LogSweeper::VERSION).not_to be(nil)
   end
+
+  def create_file!(filename, mtime = nil)
+    path = log_dir.join(filename)
+    path.write("Hello world!")
+    path.utime(mtime, mtime) if mtime
+  end
+
+  describe ".run" do
+    before do
+      log_dir.rmtree
+      log_dir.mkdir
+
+      create_file!("old file.log", Time.now - (20 * 24 * 3600))
+      create_file!("recent file.log")
+      create_file!("recent file.log.1")
+      create_file!("ignored file.log.1.smth")
+      create_file!("ignored file 2.txt")
+
+      sweeper_logger.formatter = proc { |*, msg| "#{msg}\n" }
+    end
+
+    let(:log_dir) { Pathname.new(__dir__).join("log") }
+    let(:sweeper_logger) { Logger.new(sweeper_log) }
+    let(:sweeper_log) { StringIO.new }
+
+    it "sweeps the logs" do
+      LogSweeper.run("spec/log", logger: sweeper_logger)
+
+      expect(log_dir.children.map { |x| x.basename.to_s }).to match_array([
+        "ignored file 2.txt",
+        "ignored file.log.1.smth",
+        "recent file.log.1.gz",
+        "recent file.log",
+      ])
+
+      expect(sweeper_log.tap(&:rewind).read.lines).to match_array([
+        "deleting spec/log/old file.log\n",
+        "gzipping spec/log/recent file.log.1\n",
+        "skipping spec/log/ignored file 2.txt\n",
+        "skipping spec/log/ignored file.log.1.smth\n",
+        "skipping spec/log/recent file.log\n",
+      ])
+    end
+  end
 end
