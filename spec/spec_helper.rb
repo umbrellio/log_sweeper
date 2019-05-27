f3ee61c5ddf2d0478587cb4d4b867b5c0f54ef1d
Rustam Ibragimov <33183529+0exp@users.noreply.github.com>
2019-05-27 14:10:04 +0300

    some minor improvements and updates (#1)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "bundler/setup"
 require "simplecov"
 require "coveralls"
 
@@ -10,18 +9,15 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 ])
 
 SimpleCov.minimum_coverage(100)
-SimpleCov.start
+SimpleCov.start { add_filter "spec" }
 
+require "bundler/setup"
 require "log_sweeper"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
+  config.order = :random
+  Kernel.srand config.seed
   config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
 end
